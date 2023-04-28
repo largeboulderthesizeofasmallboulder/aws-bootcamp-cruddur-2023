9a5a069a8af5e8f792fb3625d85110174b263d4f
largeboulderthesizeofasmallboulder <jajenya7@gmail.com>
2023-04-28 18:32:56 +0000

    checkAuth update

--- frontend-react-js/src/pages/MessageGroupsPage.js
+++ frontend-react-js/src/pages/MessageGroupsPage.js
@@ -3,6 +3,8 @@ import React from "react";
 
 import DesktopNavigation  from '../components/DesktopNavigation';
 import MessageGroupFeed from '../components/MessageGroupFeed';
+import checkAuth from '../lib/CheckAuth';
+
 
 export default function MessageGroupsPage() {
   const [messageGroups, setMessageGroups] = React.useState([]);
@@ -30,16 +32,16 @@ export default function MessageGroupsPage() {
     }
   };  
 
-  const checkAuth = async () => {
-    console.log('checkAuth')
-    // [TODO] Authenication
-    if (Cookies.get('user.logged_in')) {
-      setUser({
-        display_name: Cookies.get('user.name'),
-        handle: Cookies.get('user.username')
-      })
-    }
-  };
+  // const checkAuth = async () => {
+  //   console.log('checkAuth')
+  //   // [TODO] Authenication
+  //   if (Cookies.get('user.logged_in')) {
+  //     setUser({
+  //       display_name: Cookies.get('user.name'),
+  //       handle: Cookies.get('user.username')
+  //     })
+  //   }
+  // };
 
   React.useEffect(()=>{
     //prevents double call
@@ -47,7 +49,7 @@ export default function MessageGroupsPage() {
     dataFetchedRef.current = true;
 
     loadData();
-    checkAuth();
+    checkAuth(setUser);
   }, [])
   return (
     <article>
